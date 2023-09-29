ae2a573bbed63b7e4a5f9fa5a4aae9ef19bccf9a
owhslp <owhslp@icloud.com>
2023-09-30 01:19:00 +0200

    n

--- site/templates/script.js
+++ site/templates/script.js
@@ -1,26 +1,36 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var descriptionText = document.getElementById('descriptionText');
-    var backbutton = document.getElementById('backbutton');
-    var sentences = [
-        "Zdanie nr1.",
-        "Zdanie nr2.",
-        "Zdanie nqweqweqwwr3."
-    ];
+const descriptionContainer = document.getElementById("descriptionContainer");
+const descriptions = [
+    "Hello! My name is Bart, and I'm passionate about data science and programming. \n I'm currently pursuing my studies in Applied Computer Science at University in Katowice.",
+    "My Interests:",
+    "I have strong interest in data science and machine learning. I want to expand my knowledge in this field and exploring new techniques and technologies.  ", 
+    "I have a bit of experience with python and I'm continually expanding my knowledge in this language. \n It's my first language that I tryied to learn.\n And I think it was good decision :) ",
+    "In the future, I aspire to work in the field of data science, when I can apply my skills to solve real-world problems and make data-driven decisions. ",
+    "GET IN TOUCH",
+    "I'm always open to new opportunites and collaborations. Feel free to reach out to me if you'd like to connect, collaborate, or just have a chat.",
+    "You can find me on Github or LinkedIn, in previous site. ",
+    "Lets code... "
+];
 
-    var index = 0;
+let currentIndex = 0;
 
-    function displayNextSentence() {
-        if (index < sentences.length) {
-            descriptionText .textContent = sentences[index];
-            index++;
-        } else {
-            backbutton.textContent='koniec prezentacji';
-        }
+function displayNextDescription() {
+    if(currentIndex < descriptions.length){
+        const description = descriptions[currentIndex];
+        const paragraph = document.createElement("p");
+        paragraph.textContent = description;
+        paragraph.classList.add("fadeIn");
+        descriptionContainer.appendChild(paragraph);
+        currentIndex++;
+        setTimeout(displayNextDescription, 2000);
     }
-    displayNextSentence();
+}
 
-    backbutton.addEventListener('click', function() {
-        window.history.back();
-    })
+document.getElementById("backbutton").addEventListener("click", () => {
+    window.history.back();
 
 });
+
+window.onload = function () {
+    displayNextDescription();
+
+}
